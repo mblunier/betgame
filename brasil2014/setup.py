637a89d8db7eff16035946ab0ced5e0a5b981b1b
Markus Blunier <mblunier@rolotec.ch>
2014-04-06 15:04:41 +0200

    Version 0.3

--- brasil2014/setup.py
+++ brasil2014/setup.py
@@ -24,7 +24,7 @@ requires = [
     ]
 
 setup(name='brasil2014',
-      version='0.2',
+      version='0.3',
       description='brasil2014',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
