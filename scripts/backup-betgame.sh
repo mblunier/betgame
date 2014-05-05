8b6eff4900fb992a70aa0660ee0dc4123cd72446
Markus Blunier <mblunier@rolotec.ch>
2014-05-05 18:23:20 +0200

    Updated backup & control scripts.

--- scripts/backup-betgame.sh
+++ scripts/backup-betgame.sh
@@ -5,9 +5,9 @@
 TMPDIR="/tmp/betgame"
 [ -d $TMPDIR ] || mkdir $TMPDIR
 
-name=${1:-"admin"}
-pass=${2:-"None"}
-site="http://127.0.0.1:6543/"
+site=${1:-"127.0.0.1:8080"}
+name=${2:-"admin"}
+pass=${3:-"None"}
 cookies="$TMPDIR/cookies.txt"
 
 WGET="wget -v"
@@ -22,18 +22,18 @@ csrf=`grep _csrf /tmp/betgame/login_form | cut -d\" -f 6`
 #echo $csrf
 
 wget --save-cookies $cookies --keep-session-cookies --load-cookies $cookies \
-     --post-data="form.submitted&_csrf=$csrf&alias=$name&password=$pass" "${site}login" \
+     --post-data="form.submitted&_csrf=$csrf&alias=$name&password=$pass" "http://${site}/login" \
      -O /dev/null
 
-wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "${site}backup/categories" \
+wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/categories" \
      -O $DATADIR/categories.dat
-wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "${site}backup/settings" \
+wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/settings" \
      -O $DATADIR/settings.dat
-wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "${site}backup/matches" \
+wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/matches" \
      -O $DATADIR/matches.dat
-wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "${site}backup/players" \
+wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/players" \
      -O $DATADIR/players.dat
-wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "${site}backup/tips" \
+wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/tips" \
      -O $DATADIR/tips.dat
-wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "${site}backup/final" \
+wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/final" \
      -O $DATADIR/final.dat
