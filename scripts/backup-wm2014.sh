731a9dfb9b49982d2d402168fa1fddca49194a8d
Markus Blunier <mblunier@rolotec.ch>
2014-06-14 14:32:50 +0200

    Save local update log.

--- scripts/backup-wm2014.sh
+++ scripts/backup-wm2014.sh
@@ -15,18 +15,16 @@ DATADIR=$HOME/backup/$(echo ${site} | tr ':' '_')
 [ -d $DATADIR ] || mkdir -p $DATADIR
 cd $DATADIR
 
-wget --save-cookies $cookies --keep-session-cookies --load-cookies $cookies "${site}" \
-     -O $TMPDIR/login_form
+wget --save-cookies $cookies --keep-session-cookies --load-cookies $cookies "${site}" -O $TMPDIR/login_form
 
 # extract the hidden _csrf token from the login form:
 csrf=`grep _csrf /tmp/betgame/login_form | cut -d\" -f 6`
 #echo $csrf
 
 wget --save-cookies $cookies --keep-session-cookies --load-cookies $cookies \
-     --post-data="form.submitted&_csrf=$csrf&alias=$name&password=$pass" "http://${site}/login" \
-     -O /dev/null
+     --post-data="form.submitted&_csrf=$csrf&alias=$name&password=$pass" "http://${site}/login" -O /dev/null
 
-wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/update_local" -O /dev/null
+wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/update_local" -O update_local.html
 wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/categories" -O categories.dat
 wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/settings" -O settings.dat
 wget --keep-session-cookies --save-cookies $cookies --load-cookies $cookies "http://${site}/backup/matches" -O matches.dat
